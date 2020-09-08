fd53997fa72035c4da86ba3e932bb5f72ef84062
Coding Saint <codingsaint@gmail.com>
2020-09-08 23:46:47 +0530

    Day 31 Symetric Tree

--- src/tree/SymmetricTree.java
+++ src/tree/SymmetricTree.java
@@ -60,4 +60,33 @@ public class SymmetricTree {
         return node;
 
     }
+
+    /**
+     A lot better way
+     public boolean isSymmetric(TreeNode root) {
+
+     if (root == null) {
+     return true;
+     }
+
+     return isSymmetric(root.left, root.right);
+
+     }
+
+     private boolean isSymmetric(TreeNode l1, TreeNode l2) {
+
+     if (l1 == null && l2 == null) {
+     return true;
+     }
+
+     if (l1 == null || l2 == null) {
+     return false;
+     }
+
+     return l1.val == l2.val
+     && isSymmetric(l1.left, l2.right) && isSymmetric(l1.right, l2.left);
+
+     }
+
+     */
 }
