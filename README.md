07ddabbf2c3a0ebccc8d18468a3ca53d14a784b0
Coding Saint <codingsaint@gmail.com>
2020-10-24 20:59:52 +0530

    Day 77 /#100DaysofCdode

--- README.md
+++ README.md
@@ -50,6 +50,7 @@ This is a personal journey to leetcode of [Kumar Pallav](https://twitter.com/kum
 |1299|[Replace Elements with Greatest Element on Right Side](https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/)|Easy | [ReplaceElementswithGreatestElementonRightSide](https://github.com/CODINGSAINT/leetcode/blob/master/src/arrays/ReplaceElementswithGreatestElementonRightSide.java)|
 |238|[ Product of Array Except Self](https://leetcode.com/problems/product-of-array-except-self/)|Medium | [ProductofArrayExceptSelf](https://github.com/CODINGSAINT/leetcode/blob/master/src/arrays/ProductofArrayExceptSelf.java)|
 |1329|[Sort the Matrix Diagonally](https://leetcode.com/problems/sort-the-matrix-diagonally/)|Medium | [SorttheMatrixDiagonally](https://github.com/CODINGSAINT/leetcode/blob/master/src/arrays/SorttheMatrixDiagonally.java)|
+|532|[K-diff Pairs in an Array](https://leetcode.com/problems/k-diff-pairs-in-an-array/)|Medium | [KdiffPairsinanArray](https://github.com/CODINGSAINT/leetcode/blob/master/src/arrays/KdiffPairsinanArray.java)|
 
 
 ## Stack
---
+++ src/arrays/KdiffPairsinanArray.java
@@ -0,0 +1,46 @@
+package arrays;
+import java.util.*;
+//https://leetcode.com/problems/k-diff-pairs-in-an-array/
+//532. K-diff Pairs in an Array
+public class KdiffPairsinanArray {
+    public static void main(String[] args) {
+        int nums[]={1,3,1,5,4};
+        KdiffPairsinanArray k= new KdiffPairsinanArray();
+        System.out.println(k.findPairs(nums,0));
+    }
+
+    public int findPairs(int[] nums, int k) {
+        int pair=0;
+
+        Set<Integer> vals= new HashSet<>();
+
+        for(int i=0;i<nums.length;i++){
+            boolean added=vals.add(nums[i]);
+            if(k==0 && !added)
+                pair++;
+        }
+        if(k==0 && vals.size()==1 && nums.length>1) return 1;
+        if(k==0) return pair;
+        //System.out.println(vals);
+        Iterator<Integer> it = vals.iterator();
+        Set<Integer> checked=new HashSet<>();
+        while(it.hasNext()){
+            int num=it.next();
+            if(vals.contains(num+k) && !checked.contains(num+k) ){
+                pair++;
+                checked.add(num);
+                checked.add(num+k);
+
+            }if(vals.contains(num-k) && !checked.contains(num-k)  ){
+                pair++;
+                checked.add(num);
+                checked.add(num-k);
+            }
+        }
+
+
+
+
+        return pair;
+    }
+}
