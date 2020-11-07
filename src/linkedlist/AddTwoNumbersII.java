74a8653640553a260962470847225bf7d19f617d
Coding Saint <codingsaint@gmail.com>
2020-11-07 19:52:22 +0530

    Day 91\ /#100DaysofCdode

--- src/linkedlist/AddTwoNumbersII.java
+++ src/linkedlist/AddTwoNumbersII.java
@@ -5,9 +5,69 @@ import utils.Level;
 
 @LeetCode(no = 445,level = Level.MEDIUM,url = "https://leetcode.com/problems/add-two-numbers-ii/"
         ,title = "Add Two Numbers II")
-//TODO without reversing
+
 public class AddTwoNumbersII {
+    // Without Reversing
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1==null )return l2;
+        if(l2==null) return l1;
+
+        ListNode n1=l1;
+        ListNode n2=l2;
+
+        int size1=size(n1);
+        int size2=size(n2);
+        if(size1>size2){
+            l2= addZeroNodes(size1-size2 , l2);
+        }else if (size2>size1){
+            l1= addZeroNodes(size2-size1, l1);
+        }
+        int remainder=add(l1,l2);
+
+        if(remainder>0){
+            ListNode remainderNode= new ListNode(remainder);
+            remainderNode.next=l1;
+            l1= remainderNode;
+        }
+
+
+        return l1;
+    }
+    int add(ListNode n1, ListNode n2){
+
+        if(n1==null && n2==null) return 0;
+
+        int remainder= add(n1.next, n2.next);
+        n1.val+= n2.val+remainder;
+        remainder = (int) (n1.val / 10);
+        n1.val %= 10;
+        return remainder;
+
+    }
+    ListNode addZeroNodes(int count,ListNode node){
+        while(count >0){
+            ListNode headNode = new ListNode (0);
+            headNode.next= node;
+            count--;
+            node = headNode;
+        }
+        return node;
+    }
+
+    int size(ListNode node){
+        int size=0;
+        while(node!=null){
+            size++;
+            node= node.next;
+        }
+        return size;
+    }
+    ////////////////////////////////////////////////////////////
+
+
+    //Reverse Linked List Solution
+    public ListNode addTwoNumbersReverse(ListNode l1, ListNode l2) {
         l1=reverseList(l1);
         l2=reverseList(l2);
         int carry=0;
