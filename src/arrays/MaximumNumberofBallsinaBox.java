48bbd7e7b1054059956c6b5ee24da344f1796a67
Coding Saint <codingsaint@gmail.com>
2021-01-31 23:56:29 +0530

    Day 174

---
+++ src/arrays/MaximumNumberofBallsinaBox.java
@@ -0,0 +1,21 @@
+package arrays;
+import java.util.*:
+public class MaximumNumberofBallsinaBox {
+    public int countBalls(int lowLimit, int highLimit) {
+
+        int max=Integer.MIN_VALUE;
+        HashMap<Integer,Integer>freq= new HashMap<>();
+
+        for (int i = lowLimit; i <=highLimit; i++) {
+            int sum=0;
+            int current=i;
+            while(current>0){
+                sum+=current%10;
+                current=current/10;
+            }
+            freq.put(sum, freq.getOrDefault(sum,0)+1);
+            max=Integer.max(max,freq.get(sum));
+        }
+        return max;
+    }
+}
