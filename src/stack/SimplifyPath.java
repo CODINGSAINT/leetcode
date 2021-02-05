46ca7ba42dd629700c56f5f55154ac13c6ce17ce
Coding Saint <codingsaint@gmail.com>
2021-02-05 23:50:32 +0530

    Day 180

---
+++ src/stack/SimplifyPath.java
@@ -0,0 +1,26 @@
+package stack;
+import java.util.*;
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<String>();
+        for (String s : path.split("/")){
+            if (s.equals("..")){
+                if(!stack.empty()){
+                    stack.pop();
+                }
+            } else if (s.equals(".") || s.equals("")){
+                continue;
+            } else {
+                stack.push(s);
+            }
+        }
+        if (stack.empty()){
+            return "/";
+        }
+        String spath = "";
+        while(!stack.empty()){
+            spath = "/" + stack.pop() + spath;
+        }
+        return spath;
+    }
+}
