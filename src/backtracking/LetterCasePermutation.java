d6449c727028e6a1bf5563419f28bc47cc95c8a2
Coding Saint <codingsaint@gmail.com>
2021-02-16 22:57:06 +0530

    Day 190

---
+++ src/backtracking/LetterCasePermutation.java
@@ -0,0 +1,30 @@
+package backtracking;
+
+import java.util.*;
+
+public class LetterCasePermutation {
+    public List<String> letterCasePermutation(String S) {
+
+        List<String> list= new ArrayList<>();
+        list.add(S);
+        for(int i=0;i<S.toCharArray().length;i++){
+            int size=list.size();
+            for(int j=0;j<size;j++){
+                String str=list.get(j);
+                char []strChars=str.toCharArray();
+                char charAti=strChars[i];
+                if(!Character.isDigit(charAti)){
+                    if(Character.isUpperCase(charAti)){
+                        strChars[i]=Character.toLowerCase(charAti);
+                    }else if(Character.isLowerCase(charAti)){
+                        strChars[i]=Character.toUpperCase(charAti);
+                    }
+                    list.add(String.valueOf(strChars));
+                }
+
+            }
+        }
+        return list;
+
+    }
+}
